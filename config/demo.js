f44782b4b19bd1f9907203b1bf10c31ffc3fd9a2
nazaporta <nazarena@porta.com.py>
2025-06-10 19:53:45 -0300

    add head, header,  footer && home page

---
+++ config/demo.js
@@ -0,0 +1,56 @@
+const categoriesFeatured = [
+  {
+    name: "Peluches",
+    image: "/assets_front/images/categories/peluches.jpg",
+    url: "/products/catalog",
+  },
+  {
+    name: "Juguetes",
+    image: "/assets_front/images/categories/juguetes.jpg",
+    url: "/products/catalog",
+  },
+  {
+    name: "Muñecas",
+    image: "/assets_front/images/categories/munecas.jpg",
+    url: "/products/catalog",
+  },
+  {
+    name: "Personajes",
+    image: "/assets_front/images/categories/personajes.jpg",
+    url: "/products/catalog",
+  },
+];
+
+const offerProducts = [
+  {
+    id: 1,
+    name: "Peluche unicornio arcoíris",
+    image: "/assets_front/images/products/producto1.jpg",
+    oldPrice: 95000,
+    newPrice: 70000,
+  },
+  {
+    id: 2,
+    name: "Set de bloques didácticos",
+    image: "/assets_front/images/products/producto2.jpg",
+    oldPrice: 110000,
+    newPrice: 85000,
+  },
+  {
+    id: 3,
+    name: "Muñeca clásica de colección",
+    image: "/assets_front/images/products/producto3.jpg",
+    oldPrice: 150000,
+    newPrice: 120000,
+  },
+  {
+    id: 4,
+    name: "Figura articulada de superhéroe",
+    image: "/assets_front/images/products/producto4.jpg",
+    oldPrice: 130000,
+    newPrice: 95000,
+  },
+];
+  
+
+module.exports = { categoriesFeatured, offerProducts };
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const { categoriesFeatured, offerProducts } = require("../config/demo");
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Bienvenido' });
+  res.render("index", {
+    title: "Bienvenido",
+    categories: categoriesFeatured,
+    offers: offerProducts,
+  });
 });
 
 module.exports = router;
