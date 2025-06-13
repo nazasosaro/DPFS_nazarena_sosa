88d69592c3df3de23c86276ec21e88094c5371e2
nazaporta <nazarena@porta.com.py>
2025-06-12 23:56:26 -0300

    Add controller methods: details, create, and store for products.json

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,18 +1,56 @@
+const loadProducts = require("../utils/loadProducts");
+const saveProducts = require("../utils/saveProducts");
+const { v4: uuidv4 } = require("uuid"); // paquete de Node.js que genera identificadores unicos universales
+
 module.exports = {
-  catalog: (req, res) => {
-    res.render("products/productCatalog", { title: "Catálogo" });
+  // listar todos los productos
+  products: async (req, res) => {
+    const products = await loadProducts();
+    res.render("products/productCatalog", { title: "Catálogo", products });
   },
 
-  details: (req, res) => {
-    res.render("products/productDetails", { title: "Detalles" });
+  // mostrar el detalle de un producto por ID
+  details: async (req, res) => {
+    const products = await loadProducts();
+    const product = products.find(
+      (p) => String(p.id) === String(req.params.id)
+    ); // convierte a string de modo a comparar correctamente el id entero como el creado con uuid
+    if (!product) return res.status(404).send("Producto no encontrado");
+    res.render("products/productDetails", { title: req.params.name, product });
+  },
+
+  // mostrar formulario de creacion de producto
+  create: (req, res) => {
+    res.render("products/createProduct", { title: "Cargar Producto" });
+  },
+
+  // guardar producto nuevo (POST)
+  store: async (req, res) => {
+    const products = await loadProducts();
+
+    console.log(req.body);
+
+    const newProduct = {
+      id: uuidv4(),
+      name: req.body.name || "Nombre no especificado",
+      description: req.body.description || "Sin descripción",
+      image: req.body.image?.trim() || "/assets_front/images/default.jpg",
+      category: req.body.category || "Sin categoría",
+      colors: req.body.colors || "Sin colores",
+      price: parseFloat(req.body.price) || 0,
+      promotional_price: parseFloat(req.body.promotionalPrice) || 0,
+      stock: parseInt(req.body.stock) || 0,
+      status: "active",
+    };
+
+    products.push(newProduct); // se agrega el producto nuevo al array productos
+    await saveProducts(products); // se actualiza el JSON de productos con el nuevo dato
+
+    res.redirect(`/products/details/${newProduct.id}`);
   },
 
   cart: (req, res) => {
     res.render("products/productCart", { title: "Carrito" });
-  },
-
-  create: (req, res) => {
-    res.render("products/createProduct", { title: "Cargar Producto" });
   },
 
   edit: (req, res) => {
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get("/catalog", productController.catalog);
-router.get("/details", productController.details);
+router.get("/", productController.products);
+router.get("/details/:id", productController.details);
+
+router.get("/create", productController.create); // muedtra el formulario
+router.post("/", productController.store) // guardado del producto
+
 router.get("/cart", productController.cart);
-router.get("/create", productController.create);
 router.get("/edit", productController.edit);
 
 module.exports = router;
