6ba373a961ea19857ebbec9bb004329d3fc9dc06
nazaporta <nazarena@porta.com.py>
2025-06-15 11:01:12 -0300

    add user controller method: registerProcess for users.json

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,12 @@
+const bcrypt = require("bcryptjs"); // paquete de Node.js para encriptar las contrasenias
+const path = require("path");
+const loadUsers = require("../utils/loadUsers");
+const saveUsers = require("../utils/saveUsers");
+const { v4: uuidv4 } = require("uuid"); // paquete de Node.js que genera identificadores unicos universales
+
 module.exports = {
   loginForm: (req, res) => {
     res.render("users/login", { title: "Iniciar Sesión" });
-  },
-
-  registerForm: (req, res) => {
-    res.render("users/register", { title: "Registro" });
-  },
-
-  forgotPasswordForm: (req, res) => {
-    res.render("users/forgotPasssword", { title: "Reestablecer contraseña" });
   },
 
   loginProcess: (req, res) => {
@@ -16,9 +14,40 @@ module.exports = {
     res.send("Procesando login...");
   },
 
-  registerProcess: (req, res) => {
-    // Aquí iría la lógica de registro
-    res.send("Procesando registro...");
+  registerForm: (req, res) => {
+    res.render("users/register", { title: "Registro" });
+  },
+
+  registerProcess: async (req, res) => {
+    try {
+      const users = await loadUsers();
+
+      const { name, lastname, email, password } = req.body;
+
+      const newUser = {
+        id: uuidv4(),
+        name: name.trim(),
+        lastname: lastname.trim(),
+        email: email.trim().toLowerCase(),
+        password: bcrypt.hashSync(password, 10),
+        avatar: req.file
+          ? `/images/users/${req.file.filename}`
+          : "/assets_front/images/default.jpg",
+        createdAt: new Date().toISOString().split("T")[0],
+      };
+
+      users.push(newUser);
+      await saveUsers(users);
+
+      res.redirect("/users/login");
+    } catch (error) {
+      console.error("Error al registrar usuario:", error);
+      res.status(500).send("Hubo un error al registrar el usuario.");
+    }
+  },
+
+  forgotPasswordForm: (req, res) => {
+    res.render("users/forgotPasssword", { title: "Reestablecer contraseña" });
   },
 
   forgotPasswordProcess: (req, res) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const uploadUserImage = require("../middlewares/uploadUserImage");
 
 router.get("/login", userController.loginForm);
 router.post("/login", userController.loginProcess);
 
 router.get("/register", userController.registerForm);
-router.post("/register", userController.registerProcess);
+router.post(
+  "/register",
+  uploadUserImage.single("avatar"),
+  userController.registerProcess
+);
 
 router.get("/forgot-password", userController.forgotPasswordForm);
 router.post("/forgot-password", userController.forgotPasswordProcess);
