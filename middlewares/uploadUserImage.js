fb5ee7b6a1b80749a0de167611e97951159b0c28
nazaporta <nazarena@porta.com.py>
2025-06-15 10:59:35 -0300

    add middleware: uploadUserImage.js

---
+++ middlewares/uploadUserImage.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads/users");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "_" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
